7d3eb7895b9d03e594ae62318f6a40c0cc80dccd
DiegoVeronezi <diegoveronezi70@gmail.com>
2017-09-27 17:03:42 -0300

    segunda etapa (classe)

--- src/com/company/Conta.java
+++ src/com/company/Conta.java
@@ -2,4 +2,14 @@ package com.company;
 
 public class Conta {
 
+    String nome;
+    String sobreNome;
+    String sexo;
+    String senha;
+    String senha2;
+    long cpf;
+    int dia;
+    int mes;
+    int ano;
+
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,63 +6,68 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Conta minhaConta;
+        minhaConta = new Conta();
+
         Scanner s = new Scanner(System.in);
+
+
 
         //recebe o nome
         System.out.println("digite o seu nome:");
-        String nome = s.next();
+        minhaConta.nome = s.next();
 
         //recebe o sobrenome
         System.out.println("digite o seu sobrenome:");
-        String sobreNome = s.next();
+        minhaConta.sobreNome = s.next();
 
         //recebe o cpf
         System.out.println("digite o seu cpf: ");
-        long cpf = s.nextLong();
+        minhaConta.cpf = s.nextLong();
 
         //recebe dia
         System.out.println("digite o dia de seu nascimento: ");
-        int dia = s.nextInt();
+        minhaConta.dia = s.nextInt();
 
         //recebe mes
         System.out.println("digite o mês de seu nascimento: ");
-        int mes = s.nextInt();
+        minhaConta.mes = s.nextInt();
 
         //recebe ano
         System.out.println("digite o ano de seu nascimento: ");
-        int ano = s.nextInt();
+        minhaConta.ano = s.nextInt();
 
         //recebe sexo
         System.out.println("Sexo M | F");
-        String sexo = s.next();
+        minhaConta.sexo = s.next();
 
         //recebe senha
         System.out.println("digite uma senha: ");
-        String senha = s.next();
+        minhaConta.senha = s.next();
 
         //recebe a confirmação da senha
         System.out.println("confirme a senha: ");
-        String senha2 =(String) s.next();
+        minhaConta.senha2 = s.next();
 
         //teste da senha
-        while(!senha2.equals(senha)){
+        while(!minhaConta.senha2.equals(minhaConta.senha)){
 
             System.out.println("Senha não confere! Repita novamente!");
 
             //recebe senha
             System.out.println("digite uma senha: ");
-            senha = s.next();
+            minhaConta.senha = s.next();
 
             //recebe a confirmação da senha
             System.out.println("confirme a senha: ");
-            senha2 = s.next();
+            minhaConta.senha2 = s.next();
 
         }
 
         System.out.println("Senha OK!");
 
         // imprime o nome completo
-        System.out.println("Nome completo: " + nome + " " + sobreNome);
+        System.out.println("Nome completo: " + minhaConta.nome + " " + minhaConta.sobreNome);
 
 
 
